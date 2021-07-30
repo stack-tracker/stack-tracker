ac555654cfad4130ae913e8d11a0080cfd2a3768
Shay Wagner <srwagner916@gmail.com>
2021-07-29 20:01:08 -0500

    wrote user model

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/]
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  }
+
+});
+
+const User = model('User') = mongoose.model('User', userSchema);
+
+module.exports = User;
