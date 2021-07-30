407eff5196136e701c897d1097ed6571a14411f7
dcheever <cheeverdanny@gmail.com>
2021-07-30 17:18:42 -0500

    add index js in schemas , for with and without resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,7 @@
+const typeDefs = require('./typeDefs');
+
+module.exports = { typeDefs };
+// for when we have resolvers:
+// const resolvers = require('./resolvers');
+
+// module.exports = { typeDefs, resolvers };
