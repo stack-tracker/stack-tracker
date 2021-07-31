84f7f872ed8a98a142b1776d1def0796c5501ca5
dcheever <cheeverdanny@gmail.com>
2021-07-30 19:30:54 -0500

    fixed typo in server js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // serve up static assets, but don't think we have any yet
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-if (process.env.NODE_ENV === "prodcution") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
