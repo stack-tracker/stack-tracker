59c4e62c18217175264981e7501b0137304d1527
Vasilyg10 <zvgianna@gmail.com>
2021-07-31 16:46:08 -0500

    fixed typo

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const plus = <FontAwesomeIcon icon={faPlus} />
