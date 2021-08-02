aeb694256f176a307e98f3dd14cf1b5cd4b13878
Shay Wagner <srwagner916@gmail.com>
2021-08-02 18:54:25 -0500

    fixed path error

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import {LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
-
+import {LOGIN } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 function LoginForm() {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const { email, password } = formState;
