7480bae280bfb56c0a7bd5d64d543493eafad87a
dcheever <cheeverdanny@gmail.com>
2021-08-03 21:17:53 -0500

    add auth to dashboard

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -3,22 +3,29 @@ import Sidebar from '../../components/Sidebar';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER} from '../../utils/queries';
 
-
+import Auth from '../../utils/auth';
+import { BrowserRouter as Route, Redirect } from 'react-router-dom';
+import Signup from '../../pages/Signup';
 
 
 function Dashboard() {
 
     const { loading, data } = useQuery(QUERY_USER);
-    console.log(data)
+    console.log(data);
     
-    
+  if (Auth.loggedIn()) {  
   
   return(
     <div className="grid gap-6 grid-cols-3 h-screen">
       <Sidebar/>
       <span className="text-6xl text-gray-900 leading-normal col-span-2 self-center place-self-start">{loading ? "Hello" : `Welcome to your dashboard,  ${data.user.username}`}</span>
     </div>
-  )
-}
+  ) } else {
+    return (
+      <Route>
+        <Redirect to="/" /> : <Signup />
+      </Route>
+    )}
+};
 
 export default Dashboard;
