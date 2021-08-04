d3ac39a7752b4373a7269caf33413e38906dcb11
Vasilyg10 <zvgianna@gmail.com>
2021-08-03 19:50:45 -0500

    added logout login buttons

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -14,7 +14,7 @@ function LoginSignup() {
   return (
     <section className="flex flex-col h-screen justify-center">
       <div className="flex flex-row justify-center">
-        <h2 className="inline font-serif px-10 text-5xl">{ signupState ? "Sign Up!" : "Login!"} </h2>
+        <h2 id= 'login' className="inline font-serif px-10 text-5xl">{ signupState ? "Sign Up!" : "Login!"} </h2>
         <button className="inline text-xl mx-10 text-blue-600 underline" onClick={loginClickHandler}>{ signupState ? "Login" : "Sign Up"}</button>
       </div>
       { signupState ? <SignupForm /> : <LoginForm /> }
