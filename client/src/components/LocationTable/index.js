ae76a40a2b722b1cc8a9ae813ce7c79927fd7751
Shay Wagner <srwagner916@gmail.com>
2021-08-06 17:26:38 -0500

    a little reformat of locations

--- client/src/components/LocationTable/index.js
+++ client/src/components/LocationTable/index.js
@@ -59,64 +59,64 @@ function LocationTable(props) {
   }
 
   return (
-    <div className="flex flex-wrap self-center md:mx-10 md:overscroll-x-none ">
+    <div className="flex flex-wrap self-center md:mx-10 md:overscroll-x-none">
       <div className="mb-4 py-6 text-gray-900">
-        <span className="text-5xl">{locationState}</span>
+        <span className="text-5xl p-5">{locationState}:</span>
       </div>
 
-      <table className="p-4 mb-4 self-center">
+      <table className="p-4 mb-4 border-8 border-gray-900 self-center bg-gray-300">
         <thead>
-          <tr className="border-r-4 border-t-4 border-b-4 border-gray-900">
-            <th className="border-2 border-gray-900 p-3">Bankroll:</th>
-            <td className="border-2 border-gray-900 p-3">${resultSum}</td>
-            <th className="border-2 border-gray-900 p-3 ">
+          <tr>
+            <th className="border-r-4 border-gray-900 p-3">Bankroll:</th>
+            <td className="border-r-4 border-gray-900 p-3">${resultSum}</td>
+            <th className="p-3">
               Average Cash per Hour:
             </th>
-            <td className="border-2 border-gray-900 p-3">${meanCph}</td>
-            <th className="border-2 border-gray-900 p-3 ">
+            <td className="border-r-4 border-gray-900 p-3">${meanCph}</td>
+            <th className="border-r-4 border-gray-900 p-3 ">
               Average Big Blinds per Hour:
             </th>
-            <td className="border-2 border-gray-900 p-3">{meanBbph}</td>
+            <td className="p-3">{meanBbph}</td>
           </tr>
         </thead>
       </table>
 
-      <table className="p-4">
+      <table className="border-8 border-gray-900 p-4 bg-gray-300">
         <thead className="border-t-4 border-b-2 border-gray-900 text-xl text-gray-900">
-          <tr>
-            <th className="border-l-4 border-gray-900 p-3">Date</th>
-            <th className="border-l-2 border-gray-900 p-3">Hours</th>
-            <th className="border-l-2 border-gray-900 p-3">Buy In</th>
-            <th className="border-l-2 border-gray-900 p-3">Small Blind</th>
-            <th className="border-l-2 border-gray-900 p-3">Big Blind</th>
-            <th className="border-l-2 border-gray-900 p-3">Cash Out</th>
-            <th className="border-l-2 border-gray-900 p-3">Result</th>
-            <th className="border-l-2 border-gray-900 p-3">Cash Per Hour</th>
-            <th className="border-r-4 border-l-2 border-gray-900 p-3">
+          <tr className="border-b-8 border-gray-900">
+            <th className="border-r-4 border-gray-900 p-3">Date</th>
+            <th className="border-r-4 border-gray-900 p-3">Hours</th>
+            <th className="border-r-4 border-gray-900 p-3">Buy In</th>
+            <th className="border-r-4 border-gray-900 p-3">Small Blind</th>
+            <th className="border-r-4 border-gray-900 p-3">Big Blind</th>
+            <th className="border-r-4 border-gray-900 p-3">Cash Out</th>
+            <th className="border-r-4 border-gray-900 p-3">Result</th>
+            <th className="border-r-4 border-gray-900 p-3">Cash Per Hour</th>
+            <th className="p-3">
               Big Blind Per Hour
             </th>
           </tr>
         </thead>
         <tbody className="border-l-4 border-r-4 border-b-4 border-gray-900 text-lg text-gray-900">
           {locationData.map((game, i) => (
-            <tr key={i} className="border-b-2 border-gray-900">
-              <td className="border-l-2 border-gray-900 p-3">{game.date}</td>
-              <td className="border-l-2 border-gray-900 p-3">{game.hours}</td>
-              <td className="border-l-2 border-gray-900 p-3">${game.buy_in}</td>
-              <td className="border-l-2 border-gray-900 p-3">
+            <tr key={i} className="border-b-4 border-gray-900">
+              <td className="border-r-2 border-gray-900 p-3">{game.date}</td>
+              <td className="border-r-2 border-gray-900 p-3">{game.hours}</td>
+              <td className="border-r-2 border-gray-900 p-3">${game.buy_in}</td>
+              <td className="border-r-2 border-gray-900 p-3">
                 ${game.small_blind}
               </td>
-              <td className="border-l-2 border-gray-900 p-3">
+              <td className="border-r-2 border-gray-900 p-3">
                 ${game.big_blind}
               </td>
-              <td className="border-l-2 border-gray-900 p-3">
+              <td className="border-r-2 border-gray-900 p-3">
                 ${game.cash_out}
               </td>
-              <td className="border-l-2 border-gray-900 p-3">${game.result}</td>
-              <td className="border-l-2 border-gray-900 p-3">
+              <td className="border-r-2 border-gray-900 p-3">${game.result}</td>
+              <td className="border-r-2 border-gray-900 p-3">
                 ${game.cash_per_hour}
               </td>
-              <td className="border-l-2 border-gray-900 p-3">
+              <td className="p-3">
                 {game.bb_per_hour}
               </td>
             </tr>
